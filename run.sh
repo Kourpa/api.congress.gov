b5443e82406105556c1c45e8e6b152e083a22945
kourpa <kourpa@Meeseeks.>
2025-02-08 01:50:30 -0600

    run script

---
+++ run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cd ./congress
+
+python3 -m venv env
+source env/bin/activate
+
+pip install .
+
+cat << 'EOF' > config.yml
+output:
+  cache: ../cache
+  data: ../data
+EOF
+
+usc-run govinfo --bulkdata=BILLSTATUS --congress=119
+usc-run bills
+
+deactivate
+
+cd ..
